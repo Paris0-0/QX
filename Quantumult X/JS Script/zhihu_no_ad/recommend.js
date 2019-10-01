90012369c220f37a700a1f726d038c8a8618db43
jsnjycwyz <jsnjycwyz@126.com>
2019-10-01 17:55:14 +0800

    minor changes

--- Quantumult X/JS Script/zhihu_no_ad/recommend.js
+++ Quantumult X/JS Script/zhihu_no_ad/recommend.js
@@ -1,7 +1,7 @@
 let body = $response.body
 body=JSON.parse(body)
 body['data'].forEach((element, index)=> {
-    if(element['card_type']=="slot_event_card"||element.hasOwnProperty('ad')){      
+    if(element['card_type']=='slot_event_card'||element.hasOwnProperty('ad')){      
        body['data'].splice(index,1)  
     }
 })
