f303f0e9723ac74829fc59b76df3c7baf6de0189
jsnjycwyz <jsnjycwyz@126.com>
2019-10-12 12:14:06 +0800

    爱美剧

--- Quantumult X/JS Script/aimeiju.js
+++ Quantumult X/JS Script/aimeiju.js
@@ -1,8 +1,8 @@
 var body = $response.body;
 var url = $request.url;
-const path1 = "/app/ios/user/index";
-const path2 = "/app/ios/vod/show";
-const path3 = "/app/ios/ads/index";
+const path1 = "/index.php/app/ios/user/index";
+const path2 = "/index.php/app/ios/vod/show";
+const path3 = "/index.php/app/android/ads/index";
 
 if (url.indexOf(path1) != -1){
   let obj = JSON.parse(body);
@@ -21,15 +21,12 @@ if (url.indexOf(path2) != -1){
 
 if (url.indexOf(path3) != -1){
   let obj = JSON.parse(body);
-  obj.data["pic"] = "http://ww1.sinaimg.cn/large/0076dY5Wgy1g36mmbdvv7j30gf0zkwf2.jpg";
-  obj.data["url"] = "https://t.me/meetaclub";
+  obj.data["pic"] = 0;
+  obj.data["url"] = 0;
   body = JSON.stringify(obj);
 }
 
 $done({body});
 
 
-
 // Made by Meeta(爱美剧)（官网下载：app.meiju2018.com）
-// https?:\/\/mjappaz.yefu365.com\/index.php\/app\/ios\/
-// hostname = mjappaz.yefu365.com
