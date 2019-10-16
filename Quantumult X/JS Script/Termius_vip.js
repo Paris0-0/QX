11a4b9196f779d3db92b19b27343da634c9ac4ef
Across <jsnjycwyz@126.com>
2019-10-16 17:49:24 +0800

    Update Termius_vip.js

--- Quantumult X/JS Script/Termius_vip.js
+++ Quantumult X/JS Script/Termius_vip.js
@@ -2,12 +2,12 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
     obj.account.pro_mode=true;
-    obj.account.plan_type="Trial";
-    obj.account.user_type="Trial";
-    obj.account.current_period.until="2020-09-23T07:55:48";
+    obj.account.plan_type="Premium";
+    obj.account.user_type="Premium";
+    obj.account.current_period.until="2099-09-23T07:55:48";
     obj.account.team=true;
     obj.account.team_display_name="WYZ";
-    obj.trial.valid_until="2020-09-23T07:55:48";
+    obj.trial.valid_until="2099-09-23T07:55:48";
     obj.trial.is_active=true;
 
 body = JSON.stringify(obj); 
