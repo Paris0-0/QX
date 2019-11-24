2ef7a1284ba3ac502301eda8c43b83aed5c97f49
Across <jsnjycwyz@users.noreply.github.com>
2019-11-24 09:22:41 +0800

    answer.js

--- Quantumult X/JS Script/zhihu_no_ad/answer.js
+++ Quantumult X/JS Script/zhihu_no_ad/answer.js
@@ -2,13 +2,17 @@
 let body = $response.body
 body=JSON.parse(body)
 delete body['ad_info']
+/**
 body['data'].forEach((element, index)=> {
     if(element['author']['name']=="盐选推荐"){ 
           body['data'].splice(index,1)  
      }
  })
+*/
+
 body=JSON.stringify(body)
 $done({body})
+
 
 /**
  * @supported F3F1280E2817 44243A07CAD1
