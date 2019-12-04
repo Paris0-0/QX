fcafb7873b192818aff614698381baf570f2fdf7
jsnjycwyz <jsnjycwyz@126.com>
2019-12-04 15:58:04 +0800

    update

--- QuantumultX/JS_Script/practise/opendoor-cookie.js
+++ QuantumultX/JS_Script/practise/opendoor-cookie.js
@@ -3,6 +3,7 @@ var body = $request.headers;
    console.log(body);
    console.log(obj);
    let pattern = /eyJhb.+?\"\,\"/g;
-   let test = obj.match(pattern);
+   var test = obj.match(pattern);
+   obj = test[0].match(/[\w\_\-\.]+/g);
 
-$notify("点击复制Cookies", "", "jsbox://run?name=Opendoor.js&location=icloud&text="+test[0])
+$notify("点击更新开门捷径", "", "jsbox://run?name=Opendoor.js&location=icloud&text="+obj)
