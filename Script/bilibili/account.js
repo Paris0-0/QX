4e597ccf5bf1e9a2aa81289b24a98273ff2b659d
jsnjycwyz <jsnjycwyz@126.com>
2020-01-27 14:42:59 +0800

    update

--- Script/bilibili/account.js
+++ Script/bilibili/account.js
@@ -1,8 +1,27 @@
 let body = $response.body
-body=JSON.parse(body)
-body['data']['sections'].splice(2,1)
-body=JSON.stringify(body)
-$done({body})
+
+body = JSON.parse(body)
+body['data']['sections'] = [{
+	"title": "个人中心",
+	"items": [{
+		"title": "历史记录",
+		"uri": "bilibili://user_center/history"
+	}, {
+		"title": "我的收藏",
+		"uri": "bilibili://user_center/favourite"
+	}, {
+		"title": "稍后再看",
+		"uri": "bilibili://user_center/watch_later"
+	}, {
+		"title": "离线缓存",
+		"uri": "bilibili://user_center/download"
+	}, ]
+}]
+
+body = JSON.stringify(body)
+$done({
+	body
+})
 
 /**
  * @supported F3F1280E2817 44243A07CAD1
