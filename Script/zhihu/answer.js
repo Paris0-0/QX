e554312311e96645c96b18efff4c944d0950a071
jsnjycwyz <jsnjycwyz@126.com>
2020-02-23 21:52:16 +0800

    update

--- Script/zhihu/answer.js
+++ Script/zhihu/answer.js
@@ -1,14 +1,18 @@
-let body = $response.body
-body=JSON.parse(body)
-delete body['ad_info']
-body['data'].forEach((element, index)=> {
-    if(element['author']['name']=="盐选推荐"||element['author']['name']=="盐选科普"){ 
-          body['data'].splice(index,1)  
-     }
- })
-body=JSON.stringify(body)
-$done({body})
-
+let body = $response.body;
+body = JSON.parse(body);
+delete body["ad_info"];
+if (body["data"]) {
+  body["data"].forEach((element, index) => {
+    if (
+      element["author"]["name"] == "盐选推荐" ||
+      element["author"]["name"] == "盐选科普"
+    ) {
+      body["data"].splice(index, 1);
+    }
+  });
+}
+body = JSON.stringify(body);
+$done({ body });
 
 /**
  * @supported F3F1280E2817 44243A07CAD1
