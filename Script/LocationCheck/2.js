791a4dada903632733cb692ec7f35d5beef53971
jsnjycwyz <jsnjycwyz@126.com>
2020-03-28 14:02:33 +0800

    Update 2.js

--- Script/LocationCheck/2.js
+++ Script/LocationCheck/2.js
@@ -26,7 +26,7 @@ var obj = JSON.parse(body);
 
 var title =flags.get(obj['countryCode']) + ' '+ obj['country']+'，'+obj['continent'];
 
-var subtitle = obj['regionName']+ (obj['regionName']!=""? ',':'') +City_ValidCheck(obj['city']) +'-'+ ISP_ValidCheck(obj['org'],obj['isp']);
+var subtitle = obj['regionName']+ (obj['regionName']!=""? ',':'') +'-'+ ISP_ValidCheck(obj['org'],obj['isp']);
 
 var ip = obj['query'];
 
